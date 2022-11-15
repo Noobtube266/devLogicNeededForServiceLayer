e6f566d64ef0f107f1745f43ff3d28bf73bf05ab
noobtube266 <durranh12@icloud.com>
2022-11-15 12:43:26 -0500

    Not done

--- src/main/java/com/gotbizlogic/demo/of/incompleteCodeBase/services/AuthorService.java
+++ src/main/java/com/gotbizlogic/demo/of/incompleteCodeBase/services/AuthorService.java
@@ -1,13 +1,38 @@
 package com.gotbizlogic.demo.of.incompleteCodeBase.services;
 
+import com.gotbizlogic.demo.of.incompleteCodeBase.models.Author;
+import com.gotbizlogic.demo.of.incompleteCodeBase.repos.AuthorRepo;
+import com.gotbizlogic.demo.of.incompleteCodeBase.repos.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorService {
+
+    @Autowired
+    private AuthorRepo authorRepo;
+
+    @Autowired
+    private BookRepo bookRepo;
     /*
         //THIS IS THE ONLY FILE YOU SHOULD WRITE CODE
         //THIS IS WHERE YOUR LOGIC COMES INTO PLAY
         //DO NOT TOUCH THE CONTROLLER OR MODELS OR REPOS!!!
      */
+    public void verifyAuthor(Long authorId) {
+         Author author = authorRepo.findById(authorId).orElse(null);
+    }
 
     //Todo - Business Logic or Algorithm needed to create an author
+    public ResponseEntity<Author> createAuthor(Author author) {
+        author=authorRepo.save(author);
+        return new ResponseEntity<>(author, HttpStatus.OK);
+    }
     //Todo - Business Logic or Algorithm needed to GetAll authors and to also be able to Search for an author by name
+    public Iterable<Author> getAllAuthors(String authorName) {
+        return authorRepo.findByNameContaining(authorName);
+    }
 
 }
--- src/main/java/com/gotbizlogic/demo/of/incompleteCodeBase/services/BookService.java
+++ src/main/java/com/gotbizlogic/demo/of/incompleteCodeBase/services/BookService.java
@@ -1,6 +1,21 @@
 package com.gotbizlogic.demo.of.incompleteCodeBase.services;
 
+import com.gotbizlogic.demo.of.incompleteCodeBase.models.Book;
+import com.gotbizlogic.demo.of.incompleteCodeBase.repos.AuthorRepo;
+import com.gotbizlogic.demo.of.incompleteCodeBase.repos.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    @Autowired
+    private BookRepo bookRepo;
+
+    @Autowired
+    private AuthorRepo authorRepo;
 
         /*
             //THIS IS THE ONLY FILE YOU SHOULD WRITE CODE
@@ -9,9 +24,22 @@ public class BookService {
         */
 
     //Todo - Business Logic or Algorithm needed to create a book
+    public ResponseEntity<Book> createBook(Long authorId, Book bookRequest){
+        authorRepo.findById(authorId).map(author ->{
+            bookRequest.setAuthor(author);
+            return bookRepo.save(bookRequest);
+        });
+        return new ResponseEntity<>(bookRequest, HttpStatus.OK);
+    }
 
     //Todo - Business Logic or Algorithm needed to getAllBooksByAuthorId
+    public Iterable<Book> getAllBooksByAuthorId(Long authorId) {
+        return bookRepo.findAll();
+    }
 
     //Todo - Business Logic or Algorithm needed to getBookById
-
+    public ResponseEntity<?> getBookById(Long bookId) {
+        Book book = bookRepo.findById(bookId).orElse(null);
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
 }
